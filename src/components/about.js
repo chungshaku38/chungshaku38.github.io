164707fe24d7c0ae2ee99d42eb3c0397d33ea532
Chung Nguyen <chungshaku38@gmail.com>
2020-11-11 15:25:53 +0700

    add missing code

---
+++ src/components/about.js
@@ -0,0 +1,22 @@
+const { Grid, Cell, List } = require("react-mdl")
+
+const About = () => {
+    return (
+        <div className='about-container'>
+            <div className='about-grid'>
+                <Grid>
+                    <Cell col={4} className='about-content'>
+                        <List>
+
+                        </List>
+                        <h4>I love writing code, passionated technical problem solving and software architecture designing</h4>
+                        <p>I have more than 10 years working in software development, for both outsourcing and product company </p>
+                    </Cell>
+                    
+                </Grid>
+            </div>
+        </div>
+    )
+}
+
+export default About;
